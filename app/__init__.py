83c324bad95914285414d6a5e72cad00154ed645
Jesus Ordosgoitty <jodaz.business@gmail.com>
2024-10-25 01:00:34 -0400

    fix cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from dotenv import load_dotenv
 from config import Config
+from flask_cors import CORS
 
 load_dotenv()
 
 def create_app():
     app = Flask(__name__)
+
+    CORS(app)
 
     from .routes import main as main_blueprint
     app.register_blueprint(main_blueprint)
